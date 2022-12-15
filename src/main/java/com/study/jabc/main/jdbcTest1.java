b1c6222627ba979aea27060a36aa919524e52818
yejeoung <cyg99080312@gmail.com>
2022-12-15 10:31:49 +0900

    데이터베이스를 자바와 연동

--- src/main/java/com/study/jabc/main/jdbcTest1.java
+++ src/main/java/com/study/jabc/main/jdbcTest1.java
@@ -1,6 +1,9 @@
 package main.java.com.study.jabc.main;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.mysql.cj.jdbc.Driver;
 
@@ -14,8 +17,30 @@ public class jdbcTest1 {
 		
 		Connection connection = DBConnection.getInstance().getConnection();
 		
-		System.out.println(connection);
-		//경로를 찾아서 경로 이름 출력
+//		System.out.println(connection);
+//		//경로를 찾아서 경로 이름 출력
+		
+		
+		/*----------------------12/15---------------------*/
+		
+		String sql = "select * from score_mst";
+		//쿼리문 작성
+		
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			
+			System.out.println("id\t name\t\t score");
+			
+			while(rs.next()) {
+				System.out.println("id: " + rs.getInt(1) 
+				+ "\t name: " + rs.getString(2) 
+				+ "\t score: " + rs.getInt(3));
+			}
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/study/jabc/util/DBConnection.java
+++ src/main/java/com/study/jabc/util/DBConnection.java
@@ -10,42 +10,47 @@ import com.mysql.cj.jdbc.Driver;
 public class DBConnection {
 
 	private static DBConnection instance = null;
-	
-	private DBConnection() {} //생성자
-	
-	public static DBConnection getInstance() { //싱글톤
-		if(instance == null) {
+
+	private DBConnection() {
+	} // 생성자
+
+	public static DBConnection getInstance() { // 싱글톤
+		if (instance == null) {
 			instance = new DBConnection();
 		}
 		return instance;
 	}
-	
+
 	public Connection getConnection() {
 		Connection connection = null;
 		String url = null;
 		String username = null;
 		String password = null;
-		
+
 		try {
 			Class.forName(Driver.class.getName());
-			//Class.forName => 드라이브 객체 생성
+			// Class.forName => 드라이브 객체 생성
 			System.out.println("데이터베이스 드라이브 로딩 성공!");
-			
 			url = "jdbc:mysql://localhost:3306/subquery_study";
-			//데이터베이스 이름
+			// 데이터베이스 이름
+			// jdbc:mysql:// => 프로토콜
+			// subquery_study => Schemas
 			username = "root";
 			password = "root";
-			
+
 			connection = DriverManager.getConnection(url, username, password);
-			
+			// url, username, password(데이터베이스 정보들)
+			// 를 가지고 connection이라는 객체를 만들었다.
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			//객체 생성을 시작한다.
+			// 객체 생성을 시작한다.
 			System.out.println("드라이브 로딩 실패!");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("데이터베이스 연결 실패!");
 		}
 		return connection;
+		// connection을 리턴해줌으로써 연결된 객체라는 것을 알 수 있다.
 	}
 }
